9020de451f88f2ce0c5e39fd4ce9f20fbd0456e7
flionett <federica.lionetto@cern.ch>
2016-05-19 15:43:33 +0200

    fixed untracked files

--- TbUT/options/TbUTPedestal.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append( '/afs/cern.ch/work/c/cbetanco/LHCb/KeplerDev_v3r0/Tb/Kepler/../TbUT/options/python' )
-from TbUTPedestalRunner import TbUTPedestalRunner
-app=TbUTPedestalRunner()
-app.inputData="/afs/cern.ch/work/c/cbetanco/LHCb/KeplerDev_v3r0/Tb/Kepler/eos/lhcb/testbeam/ut/TemporaryData/May2016/MAMBA/M1/FanIn/Pedestal-M1-FanIn-5.dat"
-app.isAType=False
-app.eventMax=20000
-app.pedestalOutputData ='/afs/cern.ch/work/c/cbetanco/LHCb/KeplerDev_v3r0/Tb/Kepler/eos/lhcb/testbeam/ut/TemporaryData/May2016/DQM/M1/FanIn/output_17/Pedestal-M1-FanIn-5.dat'
-app.runPedestals()
