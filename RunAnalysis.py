bafae46c28e5aea7a2a6a59f36931c9e1bc67daf
flionett <federica.lionetto@cern.ch>
2016-05-20 09:34:32 +0200

    New argument in RunAnalysis.py for local/batch execution and factorised code.

--- RunAnalysis.py
+++ RunAnalysis.py
@@ -12,6 +12,7 @@ Description:
     - board (str): the board one wants to analyse (M1, M3, or M4);
     - PA (str): the pitch adapter region one wants to analyse (FanIn or FanUp);
     - DUTRun (str): optional, the physics run one wants to process (mostly for debugging).
+    - mode (str): how to run the script (local or batch)
 
 How to run it:
     First of all, type
@@ -26,11 +27,11 @@ How to run it:
     to loop over all the data of the test beam.
     For example:
 
-    python -u RunAnalysis.py --board M1 --PA FanIn --DUTRun 7 | tee LogRunAnalysis.dat
+    python -u RunAnalysis.py --board M1 --PA FanIn --DUTRun 7 --mode local(batch) | tee LogRunAnalysis.dat
     
     to analyse one physics run, or
 
-    python -u RunAnalysis.py --board M1 --PA FanIn | tee LogRunAnalysis.dat
+    python -u RunAnalysis.py --board M1 --PA FanIn --mode local(batch) | tee LogRunAnalysis.dat
     
     to analyse all of them.
 """
@@ -52,11 +53,11 @@ from ToolsForPython import *
 
 # cput = '23:59:59'
 # cput = '1439:59:59'
-cput = '143:59:59'
+# cput = '143:59:59'
 
 
 
-def RunAnalysis(board,PA,DUTRun) :
+def RunAnalysis(board,PA,DUTRun,mode) :
     # Check that the specified PA exists for the specified board (it can be that M1 has FanIn only, and so on).
     # print PADict[board]
     if PA not in PADict[board] :
@@ -68,6 +69,7 @@ def RunAnalysis(board,PA,DUTRun) :
     print 'Board:', board
     print 'PA:', PA
     print 'DUTRun:', DUTRun
+    print 'Mode:', mode
     print '===================='
     
     # Path to input data.
@@ -78,52 +80,7 @@ def RunAnalysis(board,PA,DUTRun) :
     logbook = 'Logbook/RunLogMay2016Board'+board+PA+'.csv'
     print 'Logbook: ', logbook
 
-    # Create pandas dataframe.
-    logbook_df = pd.read_csv(logbook,sep=',',header=0,names=['Sector','Purpose','Biasing scheme','DUT run','Telescope run','x (mm)','y (mm)','Rotation (deg)','Bias voltage (V)','Current (uA)','Temperature (C)','RH (%)','Online flag','Offline flag','Comments'],skiprows=1,na_values=['\n'],usecols=['Sector','Purpose','Biasing scheme','DUT run','Telescope run','x (mm)','y (mm)','Rotation (deg)','Bias voltage (V)','Current (uA)','Temperature (C)','RH (%)','Online flag','Offline flag','Comments'],skip_blank_lines=True)
-
-    print '================'
-    print 'Original logbook'
-    print '================'
-    print logbook_df.head()
-    print logbook_df.shape
-    print '================'
-
-    # Drop NaN rows (empty rows).
-    logbook_df_clean = logbook_df.dropna(axis=0,how='all')
-
-    print '================================='
-    print 'Logbook after dropping empty rows'
-    print '================================='
-    print logbook_df_clean.head()
-    print logbook_df_clean.shape
-    print '================'
-
-    # Sanitise input.
-    # bad,good -> Bad,Good
-    # empty -> -
-    # AngleScan -> Angle scan
-    # Angle Scan -> Angle scan
-    # BiasScan -> Bias scan
-    # Bias Scan -> Bias scan
-    logbook_df_san = logbook_df_clean.fillna('-')
-    logbook_df_san = logbook_df_san.replace('bad','Bad')
-    logbook_df_san = logbook_df_san.replace('good','Good')
-    logbook_df_san = logbook_df_san.replace('BiasScan','Bias scan')
-    logbook_df_san = logbook_df_san.replace('Bias Scan','Bias scan')
-    logbook_df_san = logbook_df_san.replace('AngleScan','Angle scan')
-    logbook_df_san = logbook_df_san.replace('Angle Scan','Angle scan')
-    # Delete rows without DUT run.
-    logbook_df_san = logbook_df_san.drop(logbook_df_san[logbook_df_san['DUT run'].astype(str) == '-'].index)
-    # Delete rows with bad online or offline flag. 
-    logbook_df_san = logbook_df_san.drop(logbook_df_san[logbook_df_san['Online flag'] == 'Bad'].index)
-    logbook_df_san = logbook_df_san.drop(logbook_df_san[logbook_df_san['Offline flag'] == 'Bad'].index)
-
-    print '=============================='
-    print 'Logbook after sanitising input'
-    print '=============================='
-    print logbook_df_san.head()
-    print logbook_df_san.shape
-    print '================'
+    logbook_df_san = GetInfoFromLogbook(logbook)
 
     # Environment variables.
     path = os.environ['PATH']
@@ -143,12 +100,32 @@ def RunAnalysis(board,PA,DUTRun) :
         if rowDUTRun.get_value(rowDUTRun_i,'Purpose') == 'Pedestal' or rowDUTRun.get_value(rowDUTRun_i,'Purpose') == 'Calibration' :
             print 'ERROR! This is a pedestal or calibration run.'
             exit()
+        logbook_df_subset = rowDUTRun.copy(deep=True)
+
+    else :
+        # Select runs for the loop.
+        print 'Running the analysis for all runs.'
+        logbook_df_subset = logbook_df_san.loc[(logbook_df_san['Purpose'] == 'Bias scan') | (logbook_df_san['Purpose'] == 'Angle scan') | (logbook_df_san['Purpose'] == 'Eff/Res')].copy(deep=True)
+    
+    print logbook_df_subset.head()
+    print logbook_df_subset.shape
+
+    # Loop over the selected runs.
+    print '==========================='
+    print 'Loop over the selected runs'
+    for index,row in logbook_df_subset.iterrows() :
+        # print index
+        # print row
+
+        DUTRun = int(row['DUT run'])
+        # print DUTRun
 
         print 'Running analysis for', DUTRun, 'run.'
         rowPed, rowPed_i = AssociatePed(logbook_df_san,DUTRun)
+        # print rowPed, rowPed_i
         ped = int(rowPed.get_value(rowPed_i[0],'DUT run'))
         # print ped
-
+           
         # Assign filenames for physics and pedestal runs.
         for filename in os.listdir(pathToInput) :
             if (fnmatch.fnmatch(filename,'*-'+board+'-'+PA+'-'+str(DUTRun)+'-*.dat')) :
@@ -158,63 +135,7 @@ def RunAnalysis(board,PA,DUTRun) :
                 print 'Filename for pedestal run:', filename
                 filenameNoPathPed = filename
 
-        # Use qsub
-        # command = "qsub -l cput=" + cput + " -v board="+board+",PA="+PA+",DUTRun="+str(DUTRun)+",ped="+str(ped)+",filenameNoPathPhys="+filenameNoPathPhys+",filenameNoPathPed="+filenameNoPathPed+" SubmitAnalysis.pbs" # No spaces after ','.
-        line_run = "python "+kepler+"/../Analysis.py -b "+board+" -r "+PA+" -s "+filenameNoPathPhys+" -p "+filenameNoPathPed
-        
-        with open('clusterRun.sh','w') as text_file :
-            text_file.write(line_export_path)
-            text_file.write(line_empty)
-            text_file.write(line_export_ld_library_path)
-            text_file.write(line_empty)
-            text_file.write(line_run)
-            text_file.closed
-        
-        command = 'chmod +x clusterRun.sh'
-        print '>', command
-        subprocess.call(command,shell=True,cwd='.')
-
-        command = "bsub -q 8nm clusterRun.sh"
-        print command
-        subprocess.call(command,shell=True)
-        
-        # Use python directly 
-        command = "python Analysis.py -b "+board+" -r "+PA+" -s "+filenameNoPathPhys+" -p "+filenameNoPathPed
-        print command
-        # subprocess.call(command,shell=True)
-
-    else :
-        # Select runs for the loop.
-        print 'Running the analysis for all runs.'
-        logbook_df_subset = logbook_df_san.loc[(logbook_df_san['Purpose'] == 'Bias scan') | (logbook_df_san['Purpose'] == 'Angle scan') | (logbook_df_san['Purpose'] == 'Eff/Res')].copy(deep=True)
-        print logbook_df_subset.head()
-        print logbook_df_subset.shape
-
-        # Loop over the selected runs.
-        print '==========================='
-        print 'Loop over the selected runs'
-        for index,row in logbook_df_subset.iterrows() :
-            # print index
-            # print row
-
-            DUTRun = int(row['DUT run'])
-            # print DUTRun
-
-            print 'Running analysis for', DUTRun, 'run.'
-            rowPed, rowPed_i = AssociatePed(logbook_df_san,DUTRun)
-            # print rowPed, rowPed_i
-            ped = int(rowPed.get_value(rowPed_i[0],'DUT run'))
-            # print ped
-           
-            # Assign filenames for physics and pedestal runs.
-            for filename in os.listdir(pathToInput) :
-                if (fnmatch.fnmatch(filename,'*-'+board+'-'+PA+'-'+str(DUTRun)+'-*.dat')) :
-                    print 'Filename for physics run:', filename
-                    filenameNoPathPhys = filename
-                elif (fnmatch.fnmatch(filename,'*-'+board+'-'+PA+'-'+str(ped)+'.dat')) :
-                    print 'Filename for pedestal run:', filename
-                    filenameNoPathPed = filename
-
+        if (mode == 'batch') :
             # Use qsub
             #command = "qsub -l cput=" + cput + " -v board="+board+",PA="+PA+",DUTRun="+str(DUTRun)+",ped="+str(ped)+",filenameNoPathPhys="+filenameNoPathPhys+    ",filenameNoPathPed="+filenameNoPathPed+" SubmitAnalysis.pbs" # No spaces after ','.
             line_run = "python "+kepler+"/../Analysis.py -b "+board+" -r "+PA+" -s "+filenameNoPathPhys+" -p "+filenameNoPathPed
@@ -235,10 +156,11 @@ def RunAnalysis(board,PA,DUTRun) :
             print command
             subprocess.call(command,shell=True)
             
+        elif (mode == 'local') :
             # Use python directly 
             command = "python Analysis.py -b "+board+" -r "+PA+" -s "+filenameNoPathPhys+" -p "+filenameNoPathPed
             print command
-            # subprocess.call(command,shell=True)
+            subprocess.call(command,shell=True)
 
     return
 
@@ -256,6 +178,7 @@ if __name__ == "__main__" :
     parser.add_argument('--board',required=True,choices=boardList,help='board')
     parser.add_argument('--PA',required=True,choices=PAList,help='PA')
     parser.add_argument('--DUTRun',type=int,required=False,help='DUT run (for debugging, to process one DUT run only)')
+    parser.add_argument('--mode',required=True,choices=modeList,help='mode')
 
     args = parser.parse_args()
 
@@ -263,5 +186,6 @@ if __name__ == "__main__" :
     board = args.board
     PA = args.PA
     DUTRun = args.DUTRun
+    mode = args.mode
 
-    RunAnalysis(board,PA,DUTRun)
+    RunAnalysis(board,PA,DUTRun,mode)
